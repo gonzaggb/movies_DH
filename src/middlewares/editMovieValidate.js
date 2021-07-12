2ebd8be945e160ca50e31c3b1b12d1c0939af109
gonzaggb <gonzaggb@hotmail.com>
2021-07-12 18:01:01 -0300

    agregado el genre_id a las validaciones de editMovie

--- src/middlewares/editMovieValidate.js
+++ src/middlewares/editMovieValidate.js
@@ -8,13 +8,14 @@ const validations = [
             return Movie.findOne( //Saco el nombre de la pelicula por lo que pone el usuario
                 { where: { title } }
             )
-            .then(movieFind => {
-                //si existe la pelicula y su id es diferente que el recibido por parámetros
-                if(movieFind && movieFind.id != req.params.id){
-                    return Promise.reject("La película ya existe")
-                }
-            })
-        }),          
+                .then(movieFind => {
+                    //si existe la pelicula y su id es diferente que el recibido por parámetros
+                    if (movieFind && movieFind.id != req.params.id) {
+                        return Promise.reject("La película ya existe")
+                    }
+                })
+        }),
+    body('genre_id').notEmpty().withMessage("Debe seleccionar el genero de la película"),
     body('rating').notEmpty().withMessage("El rating no puede estar vacío").bail().isNumeric().withMessage('El rating debe ser numérico'),
     body('awards').notEmpty().withMessage("Los premios no pueden estar vacíos").bail().isNumeric().withMessage('Los premios deben ser numéricos'),
     body('length').notEmpty().withMessage("El largo de la película no puede estar vacío").bail().isNumeric().withMessage('El largo de la película debe ser numérico'),
